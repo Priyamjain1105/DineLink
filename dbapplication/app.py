3b708a349479629764d2f9b2f780dfdb9368a181
Priyam Jain <139670144+Priyamjain1105@users.noreply.github.com>
2025-07-15 01:38:16 +0530

    Update app.py

--- dbapplication/app.py
+++ dbapplication/app.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__,template_folder = 'templates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:arunrockstar@database-1.cvarqhfv8crs.us-east-1.rds.amazonaws.com/restaurant'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:abc123r@database-1.cvarqhfv8crs.us-east-1.rds.amazonaws.com/restaurant'
     db.init_app(app)
 
     #import later open
